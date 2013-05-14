3964378cdd058adebe0dceb266429203c904143f
Alexandru Lazar <alex@moduscreate.com>
2013-05-14 16:28:02 +0300

    refactor and add items inside config

--- app/view/SongsPanel.js
+++ app/view/SongsPanel.js
@@ -1,58 +1,61 @@
 Ext.define('TNR.view.SongsPanel', {
-    extend:'Ext.Panel',
-    xtype :'songspanel',
+    extend : 'Ext.Panel',
+    xtype  : 'songspanel',
 
-    requires:[
+    requires : [
         'Ext.Toolbar',
         'TNR.view.SongList'
     ],
 
-    config:{
-        cls          :'list-modal',
-        showAnimation:{
-            type     :'slide',
-            duration :300,
-            direction:'up'
+    config : {
+        cls           : 'list-modal',
+        showAnimation : {
+            type      : 'slide',
+            duration  : 300,
+            direction : 'up'
         },
 
-        modal        :true,
-        centered     :true,
-        width        :400,
-        hidden       :true,
-        items        :[
+        modal    : true,
+        centered : true,
+        width    : 400,
+        hidden   : true,
+        items    : [
 
             {
-                xtype      :'container',
-                dockedItems:[
-                    {
-                        xtype :'toolbar',
-                        ui    :'light',
-                        docked:'top',
-                        layout:'hbox',
-                        title :'MY SONG LIST',
-                        items :[
-                            {
-                                xtype       :'button',
-                                cls         :'close-btn',
-                                ui          :'back',
-                                width       :100,
-                                text        :'CLOSE',
-                                docked      :'left',
-                                bubbleEvents:['closeSongsPanel'],
-                                handler     :function () {
-                                    this.fireEvent('closeSongsPanel');
+                xtype  : 'container',
+                config : {
+                    dockedItems : [
+                        {
+                            xtype  : 'toolbar',
+                            ui     : 'light',
+                            docked : 'top',
+                            layout : 'hbox',
+                            title  : 'MY SONG LIST',
+                            items  : [
+                                {
+                                    xtype        : 'button',
+                                    cls          : 'close-btn',
+                                    ui           : 'back',
+                                    width        : 100,
+                                    text         : 'CLOSE',
+                                    docked       : 'left',
+                                    bubbleEvents : ['closeSongsPanel'],
+                                    handler      : function () {
+                                        this.fireEvent('closeSongsPanel');
+                                    }
                                 }
-                            }
-                        ]
-                    }
+                            ]
+                        }
 
-                ],
-                items      :[
-                    {
-                        xtype :'songlist',
-                        height:450
-                    }
-                ]
+                    ],
+                    items       : [
+                        {
+                            xtype  : 'songlist',
+                            height : 450
+                        }
+                    ]
+                }
+
 
             }
 
